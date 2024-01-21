37608aca6aae2dbef42c3f9962f9c9b8f63b3c8d
joannacfc <chang111@msu.edu>
2024-01-21 12:36:39 -0500

    8th test

--- config.eleventy.js
+++ config.eleventy.js
@@ -2,5 +2,12 @@ module.exports = function(eleventyConfig) {
     // This will copy these folders to the output without modifying them at all
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("image");
+
+    return {
+      dir: {
+        input: "src",
+        output: "www",
+      }
+    };
   }
   
